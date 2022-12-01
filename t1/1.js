ee32ca59f979d60a1f6dc820f574e7da81ed04fd
Wei Zhen <zhen.wei@metropola.fi>
2022-12-01 22:03:26 +0200

    NONE

--- t1/1.js
+++ t1/1.js
@@ -1,8 +1,5 @@
-const div = document.querySelector('#my_list');
-const html =
-        `<p>
-            <li>First item</li>
-            <li>Second item</li>
-            <li>Third item</li>
-         </p>`;
-div.innerHTML = html;
+NONE
+
+
+
+
