cb43999f651a3a9cf98d67b4b6b1664b297dfca6
Wei Zhen <zhen.wei@metropola.fi>
2022-12-01 16:27:25 +0200

    Initial commit

---
+++ t1/1.js
@@ -0,0 +1,8 @@
+const div = document.querySelector('#my_list');
+const html =
+        `<p>
+            <li>First item</li>
+            <li>Second item</li>
+            <li>Third item</li>
+         </p>`;
+div.innerHTML = html;
