1fe9ddfa2768e6f5d28aec76943b05f2ce425548
Wei Zhen <zhen.wei@metropola.fi>
2022-12-21 17:28:45 +0200

    'use strict'; const div = document.querySelector('#target'); let html = ''; const names = ['John','Paul','Jones']; for (let i = 0; i < names.length; i++) {   html += '<li>' + names[i] + '</li>'; } div.innerHTML = html;

--- t3/3.js
+++ t3/3.js
@@ -1,2 +1,8 @@
 'use strict';
-const names = ['John', 'Paul', 'Jones'];
+const div = document.querySelector('#target');
+let html = '';
+const names = ['John','Paul','Jones'];
+for (let i = 0; i < names.length; i++) {
+  html += '<li>' + names[i] + '</li>';
+}
+div.innerHTML = html;
