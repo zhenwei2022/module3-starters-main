c66c1b17c7adebd44c2bc6f2eeff929fe96f4a2b
Wei Zhen <zhen.wei@metropola.fi>
2022-12-21 22:51:48 +0200

    'use strict'; const students = [   {     name: 'John',     id: '2345768',   },   {     name: 'Paul',     id: '2134657',   },   {     name: 'Jones',     id: '5423679',   }, ]; const target = document.querySelector("#target") target.setAttribute('class','my-list') for (let i = 0; i < students.length; i++) {   const li = document.createElement('li');   li.innerHTML= '<option value=' + students[i].id + '> '+ students[i].name + '</option>';   target.appendChild(li); };

--- t4/4.js
+++ t4/4.js
@@ -13,3 +13,10 @@ const students = [
     id: '5423679',
   },
 ];
+const target = document.querySelector("#target")
+target.setAttribute('class','my-list')
+for (let i = 0; i < students.length; i++) {
+  const li = document.createElement('li');
+  li.innerHTML= '<option value=' + students[i].id + '> '+ students[i].name + '</option>';
+  target.appendChild(li);
+};
